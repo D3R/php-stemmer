87a41fe7a6ef406c50c8f86a058f534653153208
Dom Morgan <dom@d3r.com>
2026-06-15 14:29:40 +0100

    Add rector config

---
+++ rector.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+use Rector\Config\RectorConfig;
+
+return RectorConfig::configure()
+    ->withPaths([
+        __DIR__ . '/src',
+        __DIR__ . '/test',
+    ])
+    // uncomment to reach your current PHP version
+    ->withPhpSets(
+        php85: true
+    )
+    ->withPreparedSets(
+        codeQuality: true,
+        deadCode: true,
+        typeDeclarations: true,
+    )
+    ->withComposerBased(
+        phpunit: true,
+    )
+    // ->withTypeCoverageLevel(100)
+    // ->withDeadCodeLevel(100)
+    ;
